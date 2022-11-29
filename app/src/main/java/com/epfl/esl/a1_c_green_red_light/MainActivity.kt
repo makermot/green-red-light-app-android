c43f78534dd41443945766e02c7758db80c5b6f5
makermot <79661981+makermot@users.noreply.github.com>
2022-11-29 15:24:44 +0100

    Project created

---
+++ app/src/main/java/com/epfl/esl/a1_c_green_red_light/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epfl.esl.a1_c_green_red_light
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
