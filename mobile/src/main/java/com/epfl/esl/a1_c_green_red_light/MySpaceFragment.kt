032eaca167042dc05b3dda1a66dffb5f2319cf30
makermot <79661981+makermot@users.noreply.github.com>
2022-12-06 15:02:11 +0100

    Layout my space

--- mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/MySpaceFragment.kt
+++ mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/MySpaceFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.epfl.esl.a1_c_green_red_light.databinding.FragmentLoginBinding
+import com.epfl.esl.a1_c_green_red_light.databinding.FragmentMySpaceBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +16,18 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MySpaceFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMySpaceBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_space, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_space, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MySpaceFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MySpaceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.name_app)
+
+        return binding.root
     }
 }
