63f62d5b19f728fc3e808be8b65865945796564a
makermot <79661981+makermot@users.noreply.github.com>
2022-12-13 15:18:21 +0100

    Added navigation from my_space
    added navigation to lounge and stat fragment

--- mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/LoungeFragment.kt
+++ mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/LoungeFragment.kt
@@ -6,38 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.epfl.esl.a1_c_green_red_light.databinding.FragmentLoungeBinding
 import com.google.android.gms.tasks.Tasks
 import com.google.android.gms.wearable.*
 import com.google.android.gms.wearable.Wearable
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoungeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoungeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentLoungeBinding
+    private lateinit var viewModel: SharedViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //binding = DataBindingUtil.inflate(inflater, R.layout.fragment_lounge, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
+
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.name_app)
+
         return inflater.inflate(R.layout.fragment_lounge, container, false)
     }
 
@@ -51,26 +42,6 @@ class LoungeFragment : Fragment() {
                 .getMessageClient(activity as AppCompatActivity)
             messageClient.sendMessage(it, "/command", command.toByteArray())
         } }).start()
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoungeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoungeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
 }
--- mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/MySpaceFragment.kt
+++ mobile/src/main/java/com/epfl/esl/a1_c_green_red_light/MySpaceFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.epfl.esl.a1_c_green_red_light.databinding.FragmentLoginBinding
 import com.epfl.esl.a1_c_green_red_light.databinding.FragmentMySpaceBinding
 
@@ -22,6 +23,20 @@ class MySpaceFragment : Fragment() {
         // Inflate the layout for this fragment
 
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.name_app)
+
+        binding.statButton.setOnClickListener{view: View ->
+            view.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_mySpaceFragment_to_statFragment)
+            }
+        }
+
+        binding.loungeButton.setOnClickListener{view: View ->
+            view.let {
+                Navigation.findNavController(it)
+                    .navigate(R.id.action_mySpaceFragment_to_loungeFragment)
+            }
+        }
 
         return binding.root
     }
