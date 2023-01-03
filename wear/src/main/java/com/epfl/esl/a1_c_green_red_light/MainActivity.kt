32dd34429367952093ccf7bde809d1dafa84c234
Batou1406 <baptiste.savioz@gmail.com>
2023-01-03 17:17:34 +0100

    suppress wear fragment
    It was incompatible with data API

--- wear/src/main/java/com/epfl/esl/a1_c_green_red_light/MainActivity.kt
+++ wear/src/main/java/com/epfl/esl/a1_c_green_red_light/MainActivity.kt
@@ -64,39 +64,3 @@ class   MainActivity : Activity(), DataClient.OnDataChangedListener {
             }
     }
 }
-
-
-// Version avec les fragments
-/*
-package com.epfl.esl.a1_c_green_red_light
-
-import android.app.Activity
-import android.content.pm.PackageManager
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
-import android.os.Build
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.navigation.Navigation
-import com.epfl.esl.a1_c_green_red_light.databinding.ActivityMainBinding
-import com.google.android.gms.wearable.*
-import com.google.android.gms.wearable.R
-import java.util.*
-import kotlin.concurrent.timerTask
-
-class   MainActivity : FragmentActivity() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(com.epfl.esl.a1_c_green_red_light.R.layout.activity_main)
-
-    }
-}
- */
