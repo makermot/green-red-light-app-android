83c9716cc194586d6037b48ff6a98572edff1bd6
Batou1406 <baptiste.savioz@gmail.com>
2023-01-05 12:00:27 +0100

    change green red command

--- wear/src/main/java/com/epfl/esl/a1_c_green_red_light/MainActivity.kt
+++ wear/src/main/java/com/epfl/esl/a1_c_green_red_light/MainActivity.kt
@@ -142,12 +142,11 @@ class MainActivity : Activity(), SensorEventListener, DataClient.OnDataChangedLi
     // receive Message from mobile
     override fun onDataChanged(dataEvents: DataEventBuffer) {
         print(" On Data Changed Called : ")
-        println(dataEvents)
 
         dataEvents
             .filter {it.dataItem.uri.path == "/userInfo" }
             .forEach { event ->
-                println("User Info Event")
+                println("with User Info Event")
                 val receivedImage: ByteArray = DataMapItem.fromDataItem(event.dataItem).dataMap.getByteArray("profileImage")
                 username.value = DataMapItem.fromDataItem(event.dataItem).dataMap.getString("username")
                 userImage.value = BitmapFactory.decodeByteArray(receivedImage, 0, receivedImage.size)
@@ -157,20 +156,11 @@ class MainActivity : Activity(), SensorEventListener, DataClient.OnDataChangedLi
         dataEvents
             .filter {it.dataItem.uri.path == "/command" }
             .forEach { event ->
-                println("command event")
-                val receivedTimeStamp: String = DataMapItem.fromDataItem(event.dataItem).dataMap.getString("timeStamp")
+                print(" with command event : ")
                 val receivedCommand: String = DataMapItem.fromDataItem(event.dataItem).dataMap.getString("command")
-                binding.welcomeText.text = receivedTimeStamp
-                else if(receivedCommand == "change_light"){
-                    if (light == "green") {
-                        binding.container.setBackgroundColor(
-                            ContextCompat.getColor(applicationContext, R.color.red))
-                        binding.startView.setBackgroundColor(
-                            ContextCompat.getColor(applicationContext, R.color.red))
-                        light = "red"
-                        println("change light to red")
-                    }
-                    else{
+
+                when (receivedCommand) {
+                    "green" -> {
                         binding.container.setBackgroundColor(
                             ContextCompat.getColor(applicationContext, R.color.green))
                         binding.startView.setBackgroundColor(
@@ -178,13 +168,21 @@ class MainActivity : Activity(), SensorEventListener, DataClient.OnDataChangedLi
                         light = "green"
                         println("change light to green")
                     }
+                    "red" -> {
+                        binding.container.setBackgroundColor(
+                            ContextCompat.getColor(applicationContext, R.color.red))
+                        binding.startView.setBackgroundColor(
+                            ContextCompat.getColor(applicationContext, R.color.red))
+                        light = "red"
+                        println("change light to red")
+                    }
                 }
             }
 
         dataEvents
             .filter {it.dataItem.uri.path == "/state" }
             .forEach { event ->
-                print("Heart beat received :")
+                print(" with Heart beat received :")
                 val receivedStatemachine: String? = DataMapItem.fromDataItem(event.dataItem).dataMap.getString("state")
                 // To handle null case -> should never happen btw
                 if(receivedStatemachine != null){
